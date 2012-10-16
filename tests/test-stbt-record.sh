8a79a0d24b31a0eeffb4bbc14751b39698bff5cf
David Rothlisberger <david.rothlisberger@youview.com>
2012-10-16 11:40:28 +0100

    selftests: test_record is correctly reported as failing
    This test has been failing since eda17239, which added "-v" to the call
    to "stbt record". In fact only "stbt run" takes "-v".
    
    I will fix the failure in a future commit; now at least the test is
    correctly reported as failing.

--- tests/test-stbt-record.sh
+++ tests/test-stbt-record.sh
@@ -5,13 +5,13 @@ test_record() {
         cd "$scratchdir"
         stbt-record -v -o test.py --control-recorder=file://<(
             sleep 1; echo 15; sleep 1; echo 10; sleep 1; echo 0; sleep 1;)
-    )
+    ) &&
     sed -e 's/0000-15-complete.png/videotestsrc-gamut.png/' \
         -e 's/0001-10-complete.png/videotestsrc-checkers-8.png/' \
         -e 's/0002-0-complete.png/videotestsrc-redblue.png/' \
-        "$scratchdir/test.py" > "$scratchdir/test.with-cropped-images.py"
-    mv "$scratchdir/test.with-cropped-images.py" "$scratchdir/test.py"
-    rm "$scratchdir/"{0000,0001,0002}-*-complete.png
+        "$scratchdir/test.py" > "$scratchdir/test.with-cropped-images.py" &&
+    mv "$scratchdir/test.with-cropped-images.py" "$scratchdir/test.py" &&
+    rm "$scratchdir/"{0000,0001,0002}-*-complete.png &&
 
     stbt-run -v "$scratchdir/test.py"
 }
