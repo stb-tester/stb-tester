518423b49cc48c52e6d9102f9574f859262a2893
Lewis Haley <lewis.haley@youview.com>
2013-04-04 11:56:46 +0100

    gsttemplatematch.c: using non-normalized `matchMethod` raises a warning
    stb-tester's template match "supports" the non-normalized methods
    OpenCV uses for template matching, however the resulting "heat maps" do
    not make sense within the context of STBT and the `matchThreshold`
    parameter. What is meant by this, is that `matchThreshold` is limited
    to the range [0.0..1.0f], but the pixel values for the non-normalized
    heat maps do not have a concrete upper limit, and almost definitely fall
    outside the [0.0..1.0f] range.
    
    To this end, using a non-normalized method will almost always pass through
    to the second pass as the `matchThreshold` would always be exceeded, and
    therefore it is *strongly* recommended (but not enforced) that the
    equivalent normalized version of the `matchMethod` is used instead.

--- gst/gsttemplatematch.c
+++ gst/gsttemplatematch.c
@@ -317,18 +317,24 @@ gst_templatematch_set_property (GObject * object, guint prop_id,
       switch (g_value_get_enum (value)) {
         case GST_TM_MATCH_METHOD_CV_TM_SQDIFF:
           filter->matchMethod = CV_TM_SQDIFF;
+          GST_WARNING ("It is *strongly* recommended to use \
+                  `CV_TM_SQDIFF_NORMED` instead of `CV_TM_SQDIFF`.");
           break;
         case GST_TM_MATCH_METHOD_CV_TM_SQDIFF_NORMED:
           filter->matchMethod = CV_TM_SQDIFF_NORMED;
           break;
         case GST_TM_MATCH_METHOD_CV_TM_CCORR:
           filter->matchMethod = CV_TM_CCORR;
+          GST_WARNING ("It is *strongly* recommended to use \
+                  `CV_TM_CCORR_NORMED` instead of `CV_TM_CCORR`.");
           break;
         case GST_TM_MATCH_METHOD_CV_TM_CCORR_NORMED:
           filter->matchMethod = CV_TM_CCORR_NORMED;
           break;
         case GST_TM_MATCH_METHOD_CV_TM_CCOEFF:
           filter->matchMethod = CV_TM_CCOEFF;
+          GST_WARNING ("It is *strongly* recommended to use \
+                  `CV_TM_CCOEFF_NORMED` instead of `CV_TM_CCOEFF`.");
           break;
         case GST_TM_MATCH_METHOD_CV_TM_CCOEFF_NORMED:
           filter->matchMethod = CV_TM_CCOEFF_NORMED;
