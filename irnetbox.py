stb-tester/stb-tester

commit 99e077f044789de916bb6a0f744593c12a9d20f0

Fix `make check-pylint` errors found by pep8 v1.4.2
...introduced in the previous commit, which would have been tested only
with pep8 1.0.1.

pep8 1.4.2 performs more rigorous checks than Fedora 17's pep8 1.0.1.

This fixes the following violations:

* E502 the backslash is redundant between brackets
  (stbt.py:350, irnetbox.py:297)
* E128 continuation line under-indented for visual indent
  (stbt.py:351, irnetbox.py:298)

irnetbox.py
@@ -294,15 +294,17 @@ def test_that_parse_config_understands_redrat_format():
     import StringIO
 
     # pylint: disable=C0301
-    f = StringIO.StringIO(re.sub("^ +", "", flags=re.MULTILINE, string=''\
-        """Device TestRCU
+    f = StringIO.StringIO(
+        re.sub(
+            "^ +", "", flags=re.MULTILINE, string=
+            """Device TestRCU
 
-        Note: The data is of the form <signal name> MOD_SIG <max_num_lengths> <byte_array_in_ascii_hex>.
+            Note: The data is of the form <signal name> MOD_SIG <max_num_lengths> <byte_array_in_ascii_hex>.
 
-        DOWN	MOD_SIG	16 000174F5FF60000000060000004802450222F704540D12116A464F0000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020202020202030202020202020203020202030203020202030203020302020203027F0004027F
+            DOWN	MOD_SIG	16 000174F5FF60000000060000004802450222F704540D12116A464F0000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020202020202030202020202020203020202030203020202030203020302020203027F0004027F
 
-        UP	MOD_SIG	16 000174FAFF60000000050000004803457422F7045A0D13116A00000000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020203020202020202020202020203020202020203020302030203020302020203027F0004027F
-        """))
+            UP	MOD_SIG	16 000174FAFF60000000050000004803457422F7045A0D13116A00000000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020203020202020202020202020203020202020203020302030203020302020203027F0004027F
+            """))
     config = _parse_config(f)
     assert config["DOWN"].startswith("\x00\x01\x74\xF5")
     assert config["UP"].startswith("\x00\x01\x74\xFA")

stbt.py
@@ -347,8 +347,8 @@ def get_config(key, tool=None):
 def debug(msg):
     """Print the given string to stderr if stbt run `--verbose` was given."""
     if _debug_level > 0:
-        sys.stderr.write("%s: %s\n" % \
-                (os.path.basename(sys.argv[0]), str(msg)))
+        sys.stderr.write(
+            "%s: %s\n" % (os.path.basename(sys.argv[0]), str(msg)))
 
 
 class UITestError(Exception):

commit 47aa1489b1c724647bef303852acc290d99afd99

Fix `make check` pep8 error introduced in 99e077f0
On Fedora17's pep8 1.0.1 (though not on 1.4.2) it was causing:

    E251 no spaces around keyword / parameter equals

irnetbox.py
@@ -296,7 +296,7 @@ def test_that_parse_config_understands_redrat_format():
     # pylint: disable=C0301
     f = StringIO.StringIO(
         re.sub(
-            "^ +", "", flags=re.MULTILINE, string=
+            "^ +", "", flags=re.MULTILINE, string=""
             """Device TestRCU
 
             Note: The data is of the form <signal name> MOD_SIG <max_num_lengths> <byte_array_in_ascii_hex>.