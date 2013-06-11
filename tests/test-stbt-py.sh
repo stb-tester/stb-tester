c9a69e2fcc7c79185505cc5ea027755c5d08151f
David Rothlisberger <david.rothlisberger@youview.com>
2013-06-11 16:16:58 +0100

    Cleanup: Split tests/test-stbt-run.sh into test-{match,motion,stbt-py}.sh
    "test-stbt-run.sh" was getting quite long (650 lines) and the
    motion-related tests weren't all grouped together, so it seemed a good
    idea to clean this up.

---
+++ tests/test-stbt-py.sh
@@ -0,0 +1,46 @@
+# Run with ./run-tests.sh
+
+test_get_frame_and_save_frame() {
+    cat > "$scratchdir/get-screenshot.py" <<-EOF
+	wait_for_match("videotestsrc-redblue.png", consecutive_matches=24)
+	press("15")
+	wait_for_match("videotestsrc-gamut.png", consecutive_matches=24)
+	save_frame(get_frame(), "$scratchdir/gamut.png")
+	EOF
+    stbt-run -v "$scratchdir/get-screenshot.py"
+    [ -f "$scratchdir/gamut.png" ] ||
+        fail "Screenshot '$scratchdir/gamut.png' wasn't created"
+
+    cat > "$scratchdir/match-screenshot.py" <<-EOF
+	press("15")
+	# confirm_threshold accounts for match rectangle in the screenshot.
+	wait_for_match("$scratchdir/gamut.png",
+	               match_parameters=MatchParameters(confirm_threshold=0.7))
+	EOF
+    stbt-run -v "$scratchdir/match-screenshot.py"
+}
+
+test_get_config() {
+    cat > "$scratchdir/test.py" <<-EOF
+	import stbt
+	assert stbt.get_config("global", "test_key") == "this is a test value"
+	assert stbt.get_config("special", "test_key") == \
+	    "not the global value"
+	try:
+	    stbt.get_config("global", "no_such_key")
+	    assert False
+	except ConfigurationError:
+	    pass
+	try:
+	    stbt.get_config("no_such_section", "test_key")
+	    assert False
+	except ConfigurationError:
+	    pass
+	try:
+	    stbt.get_config("special", "not_special")
+	    assert False
+	except ConfigurationError:
+	    pass
+	EOF
+    stbt-run "$scratchdir/test.py"
+}
