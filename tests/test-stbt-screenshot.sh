5fb9747d6669fa9fc1b4f9e4d95c691ffb60a883
David Rothlisberger <david.rothlisberger@youview.com>
2013-07-23 15:13:28 +0100

    stbt screenshot: Optional `filename` argument
    So that users can call `stbt screenshot` with a custom filename to avoid
    overwriting previous screenshots.

--- tests/test-stbt-screenshot.sh
+++ tests/test-stbt-screenshot.sh
@@ -5,3 +5,9 @@ test_that_stbt_screenshot_saves_file_to_disk() {
     stbt-screenshot &&
     [ -f screenshot.png ]
 }
+
+test_that_stbt_screenshot_obeys_filename_arg() {
+    cd "$scratchdir" &&
+    stbt-screenshot "my screenshot.png" &&
+    [ -f "my screenshot.png" ]
+}
