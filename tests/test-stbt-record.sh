17e9bf9daf8eabfa15b6fd3e07abc314c2bec2f5
David Rothlisberger <david.rothlisberger@youview.com>
2013-07-30 17:42:14 +0100

    test_record: Check that the screenshots were saved to disk
    `stbt record` saves screenshots to disk each time the user presses a
    remote control button. Now the unit test actually checks that those
    screenshots are being saved to disk.

--- tests/test-stbt-record.sh
+++ tests/test-stbt-record.sh
@@ -1,21 +1,18 @@
 # Run with ./run-tests.sh
 
 test_record() {
-    (
-        cd "$scratchdir"
-        stbt-record -v -o test.py --control-recorder=file://<(
-            sleep 1; echo gamut;
-            sleep 1; echo checkers-8;
-            sleep 1; echo smpte; sleep 1;)
-    ) &&
-    sed -e 's/0000-gamut-complete.png/videotestsrc-gamut.png/' \
-        -e 's/0001-checkers-8-complete.png/videotestsrc-checkers-8.png/' \
-        -e 's/0002-smpte-complete.png/videotestsrc-redblue.png/' \
-        "$scratchdir/test.py" > "$scratchdir/test.with-cropped-images.py" &&
-    mv "$scratchdir/test.with-cropped-images.py" "$scratchdir/test.py" &&
-    rm "$scratchdir/"{0000,0001,0002}-*-complete.png &&
-
-    stbt-run -v "$scratchdir/test.py"
+    cd "$scratchdir" &&
+    stbt-record -v -o test.py --control-recorder=file://<(
+        sleep 1; echo gamut;
+        sleep 1; echo checkers-8;
+        sleep 1; echo smpte; sleep 1;) &&
+    [ -f 0000-gamut-complete.png ] &&
+    [ -f 0001-checkers-8-complete.png ] &&
+    [ -f 0002-smpte-complete.png ] &&
+    cp "$testdir/videotestsrc-gamut.png" 0000-gamut-complete.png &&
+    cp "$testdir/videotestsrc-checkers-8.png" 0001-checkers-8-complete.png &&
+    cp "$testdir/videotestsrc-redblue.png" 0002-smpte-complete.png &&
+    stbt-run -v test.py
 }
 
 test_that_invalid_control_doesnt_hang_stbt_record() {
