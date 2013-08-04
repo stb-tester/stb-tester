72219bdb0947c0a8a7a35773e43e10c906a568a7
David Rothlisberger <david.rothlisberger@youview.com>
2013-08-04 17:14:07 +0100

    make check-pylint: Hide pygobject deprecation messages
    The following warning was printed for each file analysed by pylint:
    
        ** Message: pygobject_register_sinkfunc is deprecated (GstObject)
    
    This deprecated function isn't directly called by any of our code, so
    there is nothing we can do about it (except maybe porting to GStreamer
    1.0).

--- extra/pylint.sh
+++ extra/pylint.sh
@@ -14,7 +14,9 @@ pep8options() {
 ret=0
 for f in "$@"; do
     r=0
-    pylint --rcfile="$(dirname "$0")/pylint.conf" $f || r=1 ret=1
+    {
+        pylint --rcfile="$(dirname "$0")/pylint.conf" $f || r=1 ret=1 
+    } 2>&1 | grep -v 'pygobject_register_sinkfunc is deprecated'
     pep8 $(pep8options $f) $f || r=1 ret=1
     [ $r -eq 0 ] && echo "$f OK"
 done
