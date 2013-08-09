72ab49b6a8beb0c7d90ebdf84c5c295209ae803c
David Rothlisberger <david.rothlisberger@youview.com>
2013-08-09 17:27:28 +0100

    make check-pylint: Fix false negatives introduced in 72219bdb
    Surprisingly (to me, at least) the change in 72219bdb causes `$r`
    to *not* be set to `1` when `pylint` fails. The following experiment
    confirms the confusing behaviour:
    
        $ r=0; { echo hi; false || r=1; }; echo $r
        hi
        1
    
        $ r=0; { echo hi; false || r=1; } | grep -v hi; echo $r
        0

--- extra/pylint.sh
+++ extra/pylint.sh
@@ -14,9 +14,8 @@ pep8options() {
 ret=0
 for f in "$@"; do
     r=0
-    {
-        pylint --rcfile="$(dirname "$0")/pylint.conf" $f || r=1 ret=1 
-    } 2>&1 | grep -v 'pygobject_register_sinkfunc is deprecated'
+    out=$(pylint --rcfile="$(dirname "$0")/pylint.conf" $f 2>&1) || r=1 ret=1
+    echo "$out" | grep -v 'pygobject_register_sinkfunc is deprecated'
     pep8 $(pep8options $f) $f || r=1 ret=1
     [ $r -eq 0 ] && echo "$f OK"
 done
