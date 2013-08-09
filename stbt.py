stb-tester/stb-tester

commit b95dc8e7357c8ff38c2ffa5baa7dbea7f553c9f5

stbt.Display: Fix `restart_source` with the Hauppauge HDPVR
This fixes a regression introduced in 587b70e6. For details on the
HDPVR's behaviour and the desired `restart_source` functionality see
b6135eca.

Without this fix, momentary input video loss with the Hauppauge HDPVR
would result in an infinite† loop of:

    Attempting to recover from video loss: Stopping source pipeline and waiting 5s...
    Restarting source pipeline...
    Restarted source pipeline
    Attempting to recover from video loss: Stopping source pipeline and waiting 5s...

† Until the current wait_for_match's `timeout_secs` timeout, whereupon
the test script would fail.

The only reliable way we have found of recovering the HDPVR after
loss of input video is to tear down the v4l2src's entire pipeline
and create it again.

stbt.py
@@ -700,32 +700,15 @@ class Display:
             "emit-signals=true "
             "caps=video/x-raw-rgb,bpp=24,depth=24,endianness=4321,"
             "red_mask=0xFF,green_mask=0xFF00,blue_mask=0xFF0000")
-        source_pipeline_description = " ! ".join([
+        self.source_pipeline_description = " ! ".join([
             user_source_pipeline,
             "queue leaky=downstream name=q",
             "ffmpegcolorspace",
             appsink])
-        self.source_pipeline = gst.parse_launch(source_pipeline_description)
+        self.create_source_pipeline()
 
-        source_bus = self.source_pipeline.get_bus()
-        source_bus.connect("message::error", self.on_error)
-        source_bus.connect("message::warning", self.on_warning)
-        source_bus.add_signal_watch()
-
-        appsink = self.source_pipeline.get_by_name("appsink")
-        appsink.connect("new-buffer", self.on_new_buffer)
         self.last_buffer = Queue.Queue(maxsize=1)
-
-        if get_config("global", "restart_source", default="false").lower() in (
-                "1", "yes", "true", "on"):
-            # Handle loss of video (but without end-of-stream event) from the
-            # Hauppauge HDPVR capture device.
-            debug("restart_source: true")
-            source_queue = self.source_pipeline.get_by_name("q")
-            self.start_timestamp = None
-            self.underrun_timeout = None
-            source_queue.connect("underrun", self.on_underrun)
-            source_queue.connect("running", self.on_running)
+        self.underrun_timeout = None
 
         if save_video and os.path.basename(sys.argv[0]) == "stbt-run":
             if not save_video.endswith(".webm"):
@@ -753,7 +736,7 @@ class Display:
         sink_bus.add_signal_watch()
         self.appsrc = self.sink_pipeline.get_by_name("appsrc")
 
-        debug("source pipeline: %s" % source_pipeline_description)
+        debug("source pipeline: %s" % self.source_pipeline_description)
         debug("sink pipeline: %s" % sink_pipeline_description)
 
         self.source_pipeline.set_state(gst.STATE_PLAYING)
@@ -762,6 +745,26 @@ class Display:
         self.mainloop_thread = threading.Thread(target=_mainloop.run)
         self.mainloop_thread.daemon = True
         self.mainloop_thread.start()
+
+    def create_source_pipeline(self):
+        self.source_pipeline = gst.parse_launch(
+            self.source_pipeline_description)
+        source_bus = self.source_pipeline.get_bus()
+        source_bus.connect("message::error", self.on_error)
+        source_bus.connect("message::warning", self.on_warning)
+        source_bus.add_signal_watch()
+        appsink = self.source_pipeline.get_by_name("appsink")
+        appsink.connect("new-buffer", self.on_new_buffer)
+
+        if get_config("global", "restart_source", default="false").lower() in (
+                "1", "yes", "true", "on"):
+            # Handle loss of video (but without end-of-stream event) from the
+            # Hauppauge HDPVR capture device.
+            debug("restart_source: true")
+            source_queue = self.source_pipeline.get_by_name("q")
+            self.start_timestamp = None
+            source_queue.connect("underrun", self.on_underrun)
+            source_queue.connect("running", self.on_running)
 
     def get_frame(self, timeout_secs=10):
         try:
@@ -872,12 +875,13 @@ class Display:
         warn("Attempting to recover from video loss: "
              "Stopping source pipeline and waiting 5s...")
         self.source_pipeline.set_state(gst.STATE_NULL)
+        self.source_pipeline = None
         GObjectTimeout(5, self.start_source).start()
         return False  # stop the timeout from running again
 
     def start_source(self):
         warn("Restarting source pipeline...")
-        self.start_timestamp = None
+        self.create_source_pipeline()
         self.source_pipeline.set_state(gst.STATE_PLAYING)
         warn("Restarted source pipeline")
         self.underrun_timeout.start()

commit 52fb3d14fcaeaa560d375bb07bbeb7e1efe66514

stbt.py: Fix pylint errors introduced in b95dc8e7
These errors were not picked up by `make check` because of the bug fixed
in 72ab49b6.

The errors were:

    stbt.py:794: Attribute 'start_timestamp' defined outside __init__
    stbt.py:877: Attribute 'source_pipeline' defined outside __init__

stbt.py
@@ -695,6 +695,8 @@ class Display:
         self.novideo = False
         self.lock = threading.RLock()  # Held by whoever is consuming frames
         self.last_buffer = Queue.Queue(maxsize=1)
+        self.source_pipeline = None
+        self.start_timestamp = None
         self.underrun_timeout = None
 
         appsink = (