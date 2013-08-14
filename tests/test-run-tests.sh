cb9cb71f5f332f88ccf3080ed83007641f10c282
David Rothlisberger <david.rothlisberger@youview.com>
2013-08-14 14:38:24 +0100

    tests/test-run-tests.sh: Test the helper functions used by the tests
    This file was missing from commit 56aa1db9 (`test_killtree`, defined
    here, was used by `test_runner_killtree` in 56aa1db9).

---
+++ tests/test-run-tests.sh
@@ -0,0 +1,31 @@
+# Tests the helper functions, used by the tests, defined in run-tests.sh.
+# Run with ./run-tests.sh
+
+test_assert() {
+    ( assert true ) || fail "'assert true' failed"
+    ! ( assert ! true ) || fail "'assert ! true' didn't fail"
+    ! ( assert false ) || fail "'assert false' didn't fail"
+    ( assert ! false ) || fail "'assert ! false' failed"
+}
+
+test_killtree() {
+    cat > killtree-test1.sh <<-EOF
+	sh killtree-test2.sh
+	EOF
+    cat > killtree-test2.sh <<-EOF
+	sh killtree-test3.sh
+	EOF
+    cat > killtree-test3.sh <<-EOF
+	sleep 60
+	EOF
+
+    waitfor() { while ! ps -f | grep -v grep | grep "$1"; do sleep 0.1; done; }
+
+    sh killtree-test1.sh &
+    pid=$!
+    waitfor killtree-test3.sh
+    killtree $!
+    ps -f
+    ! ps -f | grep -v grep | grep -q killtree-test3.sh ||
+    fail "child process 'killtree-test3.sh' still running"
+}
