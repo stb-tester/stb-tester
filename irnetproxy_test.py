9d3fa1da8937d17953ddd21106bdcd293c216fff
David Rothlisberger <david.rothlisberger@youview.com>
2013-08-23 13:29:11 +0100

    irnetproxy_test.py: Remove tests that require `mock` library
    Because the library has to be pip-installed (see 10909cc9) which is yet
    another dependency for people to install, and all our other dependencies
    can be installed with the system package manager (yum, apt-get, etc).
    
    Instead, I added some component tests in the previous commit.

--- irnetproxy_test.py
+++ irnetproxy_test.py
@@ -1,32 +1,9 @@
-import mock
-#import socket
 import unittest
 
 import irnetproxy
 
 
-def fake_recv(data):
-    # Ugly hack to work around python scoping rules
-    data = [bytes(data)]
-
-    def recv(num):
-        bytes_to_send = min(num, 5)
-        to_send = data[0][:bytes_to_send]
-        data[0] = data[0][bytes_to_send:]
-        return to_send
-    return recv
-
-
 class ProxyTest(unittest.TestCase):
-
-    def setUp(self):
-        self.socket = mock.Mock()
-        self.socket_patcher = mock.patch('socket.socket',
-                                         return_value=self.socket)
-        self.my_socket = self.socket_patcher.start()
-
-    def tearDown(self):
-        mock.patch.stopall()
 
     def test_id_generation(self):
         proxy = irnetproxy.IRNetBoxProxy("no_address")
@@ -47,24 +24,6 @@ class ProxyTest(unittest.TestCase):
         data = "YYYY"
         self.assertEqual(proxy.replace_sequence_id(data, 1), "\x00\x01YY")
         self.assertEqual(proxy.replace_sequence_id(data, 65535), "\xff\xffYY")
-
-    def test_safe_recv(self):
-        def recv(x):
-            self.assertNotEqual(x, 0)
-            return "0" if x == 1 else ("0" * (x / 2))
-        sock = mock.Mock(recv=recv)
-        self.assertEqual(irnetproxy.safe_recv(sock, 100), "0" * 100)
-
-    def test_get_message_from_irnet(self):
-        proxy = irnetproxy.IRNetBoxProxy("no_address")
-        DATA_LENGTH = "\x00\x02"
-        MESSAGE_TYPE = "\x04"
-        self.socket.recv = fake_recv(DATA_LENGTH + MESSAGE_TYPE + "HI")
-
-        proxy.connect()
-        message_type, _, data = proxy.get_message_from_irnet()
-        self.assertEqual(message_type, 4)
-        self.assertEqual(data, "HI")
 
 
 if __name__ == "__main__":
