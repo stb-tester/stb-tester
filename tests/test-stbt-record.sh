0dfcef72a643420c0036a1eaa0a70d31ab2a7b6a
Mate Szendro <mate.szendro@youview.com>
2013-09-05 14:51:49 +0100

    stbt-record: Replace multiple wait_for_matches with a press_until_match
    If the user presses the same remote control key multiple times, then
    insert a single 'press_until_match' call to the output file instead of
    multiple 'press' and 'wait_for_match' calls; and save a single template
    image after the last keypress.

--- tests/test-stbt-record.sh
+++ tests/test-stbt-record.sh
@@ -1,6 +1,6 @@
 # Run with ./run-tests.sh
 
-test_record() {
+test_record_with_different_keys() {
     stbt-record -v -o test.py --control-recorder=file://<(
         sleep 1; echo gamut;
         sleep 1; echo checkers-8;
@@ -8,9 +8,25 @@ test_record() {
     [ -f 0001-gamut-complete.png ] &&
     [ -f 0002-checkers-8-complete.png ] &&
     [ -f 0003-smpte-complete.png ] &&
+    grep "wait_for_match" test.py &&
+    ! grep "press_until_match" test.py &&
     cp "$testdir/videotestsrc-gamut.png" 0001-gamut-complete.png &&
     cp "$testdir/videotestsrc-checkers-8.png" 0002-checkers-8-complete.png &&
     cp "$testdir/videotestsrc-redblue.png" 0003-smpte-complete.png &&
+    stbt-run -v test.py
+}
+
+test_record_with_multiple_presses_of_the_same_key() {
+    stbt-record -v -o test.py --control-recorder=file://<(
+        sleep 1; echo checkers-8;
+        sleep 1; echo checkers-8;
+        sleep 1; echo checkers-8; sleep 1;) &&
+    [ -f 0001-checkers-8-complete.png ] &&
+    [ ! -f 0002-checkers-8-complete.png ] &&
+    [ ! -f 0003-checkers-8-complete.png ] &&
+    grep "press_until_match" test.py &&
+    ! grep "wait_for_match" test.py &&
+    cp "$testdir/videotestsrc-checkers-8.png" 0001-checkers-8-complete.png &&
     stbt-run -v test.py
 }
 
