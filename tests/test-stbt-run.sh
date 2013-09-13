8973554cc7db180297f6898bcda14927267d094f
Mate Szendro <mate.szendro@youview.com>
2013-09-13 14:48:03 +0100

    stbt-run: Let a test script access its real path
    If a test script is executed with `python` (without stbt):
    - the script's directory is added to the beginning of `sys.path`, therefore
      it's possible to import a module from the script's directory;
    - the script's path is accessible through the `__file__` special variable.
    
    If a test script is executed with `stbt run`:
    - stbt-run's directory is iserted to `sys.path`, therefore it's not
      possible to import a module from the script's directory;
    - `__file__` variable contains the path of `stbt-run`.
    
    To let a test script access its real path just like it was executed without
    stb-tester, add the script's directory to `sys.path`, and override `__file__`
    variable to contain the test script's path.

--- tests/test-stbt-run.sh
+++ tests/test-stbt-run.sh
@@ -11,3 +11,13 @@ test_extra_arguments() {
     ! stbt-run -v test.py smpte "$testdir/videotestsrc-checkers-8.png" &&
     stbt-run -v test.py checkers-8 "$testdir/videotestsrc-checkers-8.png"
 }
+
+test_script_accesses_its_path() {
+    touch module.py
+    cat > test.py <<-EOF
+	import module
+	print '__file__: ' + __file__
+	EOF
+
+    stbt-run -v test.py && cat log | grep '__file__: test.py'
+}
