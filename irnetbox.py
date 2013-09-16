stb-tester/stb-tester

commit 92164a9c2c9f396ca985e54e68a45f4b6697aef2

stbt.press: Wait for irNetBox "Asynchronous IR Output Complete" message
This eliminates one source of latency variability when using a script
like this to measure performance of the system under test:

    press("channel_up")
    start = time.time()
    wait_for_match("new channel.png")
    end = time.time()
    print "duration: %f" % (end - start)

This still has a low precision, limited to how long it takes
`wait_for_match` to process a single frame†. But at least it guarantees
that the time between the system-under-test receiving the infrared
signal and our `start = time()` call, is at most the time it takes for
the confirmation message to travel from the irNetBox to our PC. Since
the irNetBox and the PC are probably on the same local network this
should be minimal (compared to the time it takes a set-top box to change
channels, for example); at least it's more predictable than that same
network delay *plus* the irNetBox's delay in sending the infrared
signal.

This will hold open a TCP connection to the irNetBox until the
asynchronous completion message is received; other clients won't be able
to use the irNetBox during that time because the irNetBox only accepts a
single TCP connection at a time. If this is a problem, use the
`irnetbox-proxy` script in this same directory.

Note that I removed the `sleep(0.5)` from `stbt.IRNetBoxRemote.press`.
This isn't necessary now that we wait for the irNetBox to have sent the
infrared signal before sending the next request. I've tested against my
YouView set-top box, with a series of `stbt.press` calls one after the
other (sending 1 0 3 to change channel, or 2 2 2 to enter the letter "C"
in a text box) and the set-top box registers all keypresses correctly.

† There is work in progress to improve performance measurements, by
processing *every* frame (instead of dropping the frames we can't
process in real time) and by using the frame timestamps from the video
stream instead of wall time. See

irnetbox.py
@@ -173,12 +173,23 @@ class IRNetBox:
         if response_type == MessageTypes.ERROR:
             raise Exception("IRNetBox returned ERROR")
         if response_type == MessageTypes.OUTPUT_IR_ASYNC:
-            _, error_code, ack = struct.unpack(
+            sequence_number, error_code, ack = struct.unpack(
                 # Sequence number in the ACK message is defined as big-endian
                 # in §5.1 and §6.1.2, but experimentation shows it to be
                 # little-endian.
                 '<HBB', response_data)
-            if ack != 1:
+            if ack == 1:
+                async_type, async_data = self._responses.next()
+                if async_type != MessageTypes.IR_ASYNC_COMPLETE:
+                    raise Exception(
+                        "IRNetBox returned unexpected message %d" % async_type)
+                (async_sequence_number,) = struct.unpack(">H", async_data)
+                if async_sequence_number != sequence_number:
+                    raise Exception(
+                        "IRNetBox returned message IR_ASYNC_COMPLETE "
+                        "with unexpected sequence number %d (expected %d)" %
+                        (async_sequence_number, sequence_number))
+            else:
                 raise Exception(
                     "IRNetBox returned NACK (error code: %d)" % error_code)
         if response_type == MessageTypes.DEVICE_VERSION:
@@ -258,8 +269,7 @@ def _read_responses(stream):
             response_type, response_data = struct.unpack(
                 ">B%ds" % data_len,
                 buf[2:(3 + data_len)])
-            if response_type != MessageTypes.IR_ASYNC_COMPLETE:
-                yield response_type, response_data
+            yield response_type, response_data
             buf = buf[(3 + data_len):]
 
 

tests/fake-irnetbox
@@ -51,7 +51,7 @@ class FakeIRNetBox(SocketServer.BaseRequestHandler):
                     MessageTypes.ERROR,
                     struct.pack("B", 40))  # error code = not enough memory
             else:
-                ack = self.server.response == "ack"
+                ack = self.server.response in ["ack", "wait"]
                 self.send(
                     MessageTypes.OUTPUT_IR_ASYNC,
                     # Sequence number in the ACK message is defined as
@@ -64,7 +64,7 @@ class FakeIRNetBox(SocketServer.BaseRequestHandler):
                         1 if ack else 0))
                 print "fake-irnetbox: ...(%s)" % ("ack" if ack else "nack")
                 if ack:
-                    time.sleep(0.2)
+                    time.sleep(2 if self.server.response == "wait" else 0.2)
                     self.send(
                         MessageTypes.IR_ASYNC_COMPLETE,
                         struct.pack(">H", sequence_number))

stbt.py
@@ -1245,7 +1245,6 @@ class IRNetBoxRemote:
         with self._connect() as irnb:
             irnb.irsend_raw(
                 port=self.output, power=100, data=self.config[key])
-        time.sleep(0.5)
         debug("Pressed " + key)
 
     def _connect(self):

tests/test-irnetbox.sh
@@ -73,6 +73,25 @@ test_that_press_fails_on_irnetbox_nack() {
         fail "Didn't receive IRNetBox NACK"
 }
 
+test_that_press_waits_for_irnetbox_async_complete() {
+    start_fake_irnetbox wait
+
+    cat > test.py <<-EOF
+	import time
+	print "Before press: %d" % time.time()
+	press("MENU")
+	print "After press: %d" % time.time()
+	EOF
+    stbt-run -v \
+        --control irnetbox:localhost:$irnetbox_port:1:"$testdir"/irnetbox.conf \
+        test.py || return
+
+    before=$(cat log | awk '/Before press/ {print $3}')
+    after=$(cat log | awk '/After press/ {print $3}')
+    [[ $((after - before)) -gt 1 ]] ||
+        fail "'press' returned too quickly (before: $before; after: $after)"
+}
+
 test_irnetbox_proxy() {
     start_fake_irnetbox
     proxy_port=5887

commit e052166bd0845d10f67981d4d1b80ca41c70cdbc

irnetbox.py: "Asynchronous IR Output Complete" payload contains 4 bytes
Section 6.1.3 of the spec† says "the only data it contains is the
message sequence number of the IR Output message as a ushort" but in
practice the irNetBox sends 4 bytes, not 2, of data in this message.
The last 2 bytes are both "0".

This fixes the error "unpack requires a string argument of length 2"
introduced in 92164a9c. Before 92164a9c we ignored the contents of
this message so this bug didn't manifest.

† http://www.redrat.co.uk/products/IRNetBox_Comms-V3.0.pdf

irnetbox.py
@@ -186,7 +186,7 @@ class IRNetBox:
                 if async_type != MessageTypes.IR_ASYNC_COMPLETE:
                     raise Exception(
                         "IRNetBox returned unexpected message %d" % async_type)
-                (async_sequence_number,) = struct.unpack(">H", async_data)
+                (async_sequence_number,) = struct.unpack(">H", async_data[:2])
                 if async_sequence_number != sequence_number:
                     raise Exception(
                         "IRNetBox returned message IR_ASYNC_COMPLETE "

tests/fake-irnetbox
@@ -69,7 +69,7 @@ class FakeIRNetBox(SocketServer.BaseRequestHandler):
                     time.sleep(2 if self.server.response == "wait" else 0.2)
                     self.send(
                         MessageTypes.IR_ASYNC_COMPLETE,
-                        struct.pack(">H", sequence_number))
+                        struct.pack(">HBB", sequence_number, 0, 0))
 
         self.handle()
 