63d7e7d5f05222f45b6b73f4d7d652a8bdaa0d58
David Rothlisberger <david.rothlisberger@youview.com>
2013-10-30 12:34:08 +0000

    Revert "Remove extra/stbt_checker.py"
    This reverts commit 97eada336b11e9730506cef2452d03622f967080.
    
    I had reverted this because it doesn't work with pylint 1.0. However,
    not much *does* work with pylint 1.0†, and this tool is still useful
    for those of us with pylint 0.x still installed.
    
    † https://bitbucket.org/logilab/pylint/issue/47/

---
+++ extra/stbt_checker.py
@@ -0,0 +1,90 @@
+"""pylint plugin to do static analysis on stbt scripts
+
+* Identifies broken image links in parameters to `stbt.wait_for_match` etc.
+
+Add this file to your PYTHONPATH. Then run pylint with
+`--load-plugins=stbt_checker`, or add `load-plugins=stbt_checker` to the
+`[MASTER]` section of your pylint config file.
+
+Documentation on Abstract Syntax Tree traversal with python/pylint:
+
+* http://www.logilab.org/card/pylint_manual#writing-your-own-checker
+* http://hg.logilab.org/review/pylint/file/default/examples/custom.py
+* http://docs.python.org/2/library/ast.html
+
+"""
+
+import os
+import re
+
+# Install `pylint` to get `logilab.astng` modules.
+from logilab.astng.node_classes import BinOp, CallFunc, Getattr
+from pylint.checkers import BaseChecker
+
+try:  # >= pylint 1.0
+    from pylint.interfaces import IAstroidChecker  # pylint: disable=E0611
+except ImportError:  # < pylint 1.0
+    from pylint.interfaces import IASTNGChecker as IAstroidChecker  # pylint: disable=E0611,C0301
+
+
+class StbtChecker(BaseChecker):
+    __implements__ = IAstroidChecker  # pylint: disable=F0220
+    name = 'stbt-checker'
+    msgs = {
+        # Range 70xx reserved for custom checkers: www.logilab.org/ticket/68057
+        'E7001': ('Image "%s" not found on disk',
+                  'Used when the image path given to `stbt.wait_for_match` '
+                  'etc. does not exist on disk.'),
+    }
+
+    def visit_const(self, node):
+        if (type(node.value) is str and
+                re.search(r'.png$', node.value) and
+                not _is_calculated_value(node) and
+                not _is_pattern_value(node) and
+                not _is_whitelisted_name(node.value) and
+                not _in_whitelisted_functions(node) and
+                not _file_exists(node.value, node)):
+            self.add_message('E7001', node=node, args=node.value)
+
+
+def _is_calculated_value(node):
+    return (
+        type(node.parent) is BinOp or
+        (type(node.parent) is CallFunc and
+         type(node.parent.func) is Getattr and
+         node.parent.func.attrname == 'join'))
+
+
+def _is_pattern_value(node):
+    return re.search(r'\*', node.value)
+
+
+def _is_whitelisted_name(filename):
+    return filename == 'screenshot.png'
+
+
+def _in_whitelisted_functions(node):
+    return (
+        type(node.parent) is CallFunc and
+        node.parent.func.as_string() in (
+            "re.match",
+            "re.search",
+            "stbt.save_frame",
+        ))
+
+
+def _file_exists(filename, node):
+    """True if `filename` is found on stbt's image search path
+
+    (See commit 4e5cd23c.)
+    """
+    if os.path.isfile(os.path.join(
+            os.path.dirname(node.root().file),
+            filename)):
+        return True
+    return False
+
+
+def register(linter):
+    linter.register_checker(StbtChecker(linter))
