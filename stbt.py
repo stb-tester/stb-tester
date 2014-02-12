3ed80f593984ed58ca8150861367d5b55c6f4021
William Manley <will@stb-tester.com>
2014-02-12 11:10:46 +0000

    OCR: Silence miscellaneous output
    Otherwise every time you use OCR you get:
    
        Tesseract Open Source OCR Engine v3.02.01 with Leptonica
    
    printed in your logs. With this change logs are clearer and it's easier
    to write interactive stbt scripts. This was required as the future
    calibration program `stbt camera calibrate` is interactive in the
    terminal and uses OCR.

--- stbt.py
+++ stbt.py
@@ -699,8 +699,9 @@ def ocr(frame=None, region=None, mode=OcrMode.PAGE_SEGMENTATION_WITHOUT_OSD):
 
     with mktmp(suffix=".png") as ocr_in, mktmp(suffix=".txt") as ocr_out:
         cv2.imwrite(ocr_in.name, frame)
-        subprocess.check_call([
-            "tesseract", ocr_in.name, ocr_out.name[:-4], "-psm", str(mode)])
+        subprocess.check_output([
+            "tesseract", ocr_in.name, ocr_out.name[:-4], "-psm", str(mode)],
+            stderr=subprocess.STDOUT)
         return ocr_out.read().strip()
 
 
