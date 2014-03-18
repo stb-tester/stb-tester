ae6b6b3b82080316ffc1580e793023b8585c0d1b
David Rothlisberger <david.rothlisberger@youview.com>
2014-03-18 16:27:13 +0000

    stbt.{Match,Motion}Timeout debug output shows values < 1
    If you used `wait_for_match(..., timeout_secs=0.5)` the MatchTimeout
    exception's message was "Didn't find match for ... within 0 seconds".
    
    Note that stbt doesn't currently guarantee to process every frame
    of video, so very small values for timeout_secs aren't as useful as you
    might think. stbt does guarantee to process at least one frame (even if
    `timeout_secs=0`).

--- stbt.py
+++ stbt.py
@@ -820,7 +820,7 @@ class MatchTimeout(UITestFailure):
         self.timeout_secs = timeout_secs
 
     def __str__(self):
-        return "Didn't find match for '%s' within %d seconds." % (
+        return "Didn't find match for '%s' within %g seconds." % (
             self.expected, self.timeout_secs)
 
 
@@ -838,7 +838,7 @@ class MotionTimeout(UITestFailure):
         self.timeout_secs = timeout_secs
 
     def __str__(self):
-        return "Didn't find motion%s within %d seconds." % (
+        return "Didn't find motion%s within %g seconds." % (
             " (with mask '%s')" % self.mask if self.mask else "",
             self.timeout_secs)
 
