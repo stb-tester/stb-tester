c442d1fa5261358bff33d2efba27e6f0b1d99181
David Röthlisberger <david@rothlis.net>
2014-03-28 14:34:55 +0000

    test_teradek_vidiu: Avoid infinite loop if crtmpserver fails to start
    Wait for up to 10 seconds instead.

--- tests/hardware/teradek-vidiu.sh
+++ tests/hardware/teradek-vidiu.sh
@@ -9,12 +9,18 @@ case $1 in
     start)
         sed "s,@SCRATCH_DIR@,$scratch_dir,g" \
             $testdir/hardware/crtmpserver.lua.in >"crtmpserver.lua"
+
         crtmpserver "crtmpserver.lua" &
+        RTMPD_PID=$!
+
         # Wait for rtmp server to start up:
-        while ! netstat -ln --tcp | grep -q ':1935'; do
+        started=false
+        for i in $(seq 100); do
+            netstat -ln --tcp | grep -q ':1935' && { started=true; break; }
             sleep 0.1
         done
-        RTMPD_PID=$!
+        $started || fail "Failed to start crtmpserver"
+
         echo "$RTMPD_PID" >"$scratch_dir/crtmpserver.pid"
         set_config global.source_pipeline \
             "rtmpsrc location=rtmp://localhost/live/$STREAM_NAME\ live=1 ! decodebin"
