stb-tester/stb-tester

commit a64e7ac7a19221e7e6cc8214d3f33704812ed182

Remotes: Add support for sending keys to X11
Can be used with GStreamer's ximagesrc for desktop automation, testing
applications, websites and set-top-box UIs written in HTML5.

stbt.py
@@ -2118,6 +2118,7 @@ def uri_to_remote(uri, display):
              (:(?P<port>\d+))?
              :(?P<output>\d+)
              :(?P<config>[^:]+)''', IRNetBoxRemote),
+        (r'x11:(?P<display>.+)?', _X11Remote),
     ]
     for regex, factory in remotes:
         m = re.match(regex, uri, re.VERBOSE | re.IGNORECASE)
@@ -2345,6 +2346,19 @@ class _SamsungTCPRemote(object):
 
 def _new_samsung_tcp_remote(hostname, port):
     return _SamsungTCPRemote(_connect_tcp_socket(hostname, int(port or 55000)))
+
+
+class _X11Remote(object):
+    """Simulate key presses using xdotool.
+    """
+    def __init__(self, display=None):
+        self.display = display
+
+    def press(self, key):
+        e = os.environ.copy()
+        if self.display is not None:
+            e['DISPLAY'] = self.display
+        subprocess.check_call(['xdotool', 'key', key], env=e)
 
 
 def uri_to_remote_recorder(uri):
@@ -2716,6 +2730,32 @@ def test_samsung_tcp_remote():
     assert len(sent_data) == 2
     assert sent_data[1] == (
         b'\x00\x13\x00iphone.iapp.samsung\r\x00\x00\x00\x00\x08\x00S0VZXzA=')
+
+
+def test_x11_remote():
+    from nose.plugins.skip import SkipTest
+    from distutils.spawn import find_executable
+    if not find_executable('Xorg') or not find_executable('xterm'):
+        raise SkipTest("Testing X11Remote requires X11 and xterm")
+    if os.path.exists('/tmp/.X11-unix/X99'):
+        raise SkipTest("There is already a display server running on :99")
+
+    with _named_temporary_directory() as tmp:
+        x11 = subprocess.Popen(
+            ['Xorg', '-logfile', './99.log', '-config',
+             os.path.dirname(__file__) + '/tests/xorg.conf', ':99'], cwd=tmp)
+        while not os.path.exists('/tmp/.X11-unix/X99'):
+            time.sleep(0.1)
+        xterm = subprocess.Popen(['xterm'], env={'DISPLAY': ':99'}, cwd=tmp)
+        # Wait for xterm to get ready to process keyboard input (sorry):
+        time.sleep(1)
+        r = uri_to_remote('x11::99', None)
+        for k in ['t', 'o', 'u', 'c', 'h', 'space', 'g', 'o', 'o', 'd',
+                  'Return', 'e', 'x', 'i', 't', 'Return']:
+            r.press(k)
+        xterm.wait()
+        assert os.path.exists(tmp + '/good')
+        x11.terminate()
 
 
 def test_uri_to_remote():

.travis.yml
@@ -25,6 +25,9 @@ install:
         tesseract-ocr
         tesseract-ocr-deu
         tesseract-ocr-eng
+        xdotool
+        xserver-xorg-video-dummy
+        xterm
  - sudo pip install isort
  - git clone http://git.chromium.org/webm/webminspector.git ~/webminspector
  - make

extra/debian/control
@@ -25,7 +25,8 @@ Build-Depends: curl,
                python-yaml,
                tesseract-ocr,
                tesseract-ocr-deu,
-               tesseract-ocr-eng
+               tesseract-ocr-eng,
+               xdotool
 
 Package: stb-tester
 Architecture: any
@@ -50,7 +51,8 @@ Depends: ${shlibs:Depends},
          python-opencv,
          snmp,
          tesseract-ocr,
-         tesseract-ocr-eng
+         tesseract-ocr-eng,
+         xdotool
 Description: Automated User Interface testing for set-top boxes. 
  stb-tester tests a set-top-box by issuing commands to it using a remote-control
  and checking that it has done the right thing by analysing what is on screen.

stbt-completion
@@ -180,7 +180,7 @@ _stbt_control_uri() {
         --control=vr:) _stbt_hostname;;
         *)
             compgen -W "$( \
-                    _stbt_no_space irnetbox: lirc: vr: samsung:
+                    _stbt_no_space irnetbox: lirc: vr: samsung: x11:
                     _stbt_trailing_space none test)" \
                 -- "$cur";;
     esac

tests/xorg.conf
@@ -1,0 +1,60 @@
+# This xorg configuration file to start a dummy X11 server.
+# For details, please see:
+# https://xpra.org/Xdummy.html
+
+Section "ServerFlags"
+  Option "DontVTSwitch" "true"
+  Option "AllowMouseOpenFail" "true"
+  Option "PciForceNone" "true"
+  Option "AutoEnableDevices" "false"
+  Option "AutoAddDevices" "false"
+EndSection
+
+Section "InputDevice"
+  Identifier "dummy_mouse"
+  Option "CorePointer" "true"
+  Driver "void"
+EndSection
+
+Section "InputDevice"
+  Identifier "dummy_keyboard"
+  Option "CoreKeyboard" "true"
+  Driver "void"
+EndSection
+
+Section "Device"
+  Identifier "dummy_videocard"
+  Driver "dummy"
+  Option "ConstantDPI" "true"
+  #VideoRam 4096000
+  #VideoRam 256000
+  VideoRam 192000
+EndSection
+
+Section "Monitor"
+  Identifier "dummy_monitor"
+  HorizSync   5.0 - 1000.0
+  VertRefresh 5.0 - 200.0
+  # Small screen forcing xterm to get focus
+  Modeline "320x240" 5.26 320 304 336 352 240 241 244 249
+EndSection
+
+Section "Screen"
+  Identifier "dummy_screen"
+  Device "dummy_videocard"
+  Monitor "dummy_monitor"
+  DefaultDepth 24
+  SubSection "Display"
+    Viewport 0 0
+    Depth 24
+    Modes "320x240"
+    Virtual 320 240
+  EndSubSection
+EndSection
+
+Section "ServerLayout"
+  Identifier   "dummy_layout"
+  Screen       "dummy_screen"
+  InputDevice  "dummy_mouse"
+  InputDevice  "dummy_keyboard"
+EndSection

commit fc1ac1d825c9dc0480bb35905c3e47d5ca0431f1

Tidy up and fix unreliable test `test_x11_remote`
It was failing intermittently on Travis due to not knowing when xterm
is ready to accept key events.  Instead we keep sending key events until
we can see that they are having an effect.

This also tidies up the test somewhat.  By pulling the starting/stopping
of the X server out into a context manager it is clearer exactly what
`test_x11_remote` is intended to test, and how it tries to test it.

The broken test was introduced in a64e7ac7.

stbt.py
@@ -2737,39 +2737,47 @@ def test_samsung_tcp_remote():
         b'\x00\x13\x00iphone.iapp.samsung\r\x00\x00\x00\x00\x08\x00S0VZXzA=')
 
 
+@contextmanager
+def temporary_x_session():
+    from nose.plugins.skip import SkipTest
+    if os.path.exists('/tmp/.X11-unix/X99'):
+        raise SkipTest("There is already a display server running on :99")
+    with _named_temporary_directory() as tmp:
+        x11 = subprocess.Popen(
+            ['Xorg', '-logfile', './99.log', '-config',
+             os.path.dirname(__file__) + '/tests/xorg.conf', ':99'],
+            cwd=tmp, stderr=open('/dev/null', 'w'))
+        while not os.path.exists('/tmp/.X11-unix/X99'):
+            assert x11.returncode is None
+            time.sleep(0.1)
+        try:
+            yield ':99'
+        finally:
+            x11.terminate()
+
+
 def test_x11_remote():
     from nose.plugins.skip import SkipTest
     from distutils.spawn import find_executable
     if not find_executable('Xorg') or not find_executable('xterm'):
         raise SkipTest("Testing X11Remote requires X11 and xterm")
-    if os.path.exists('/tmp/.X11-unix/X99'):
-        raise SkipTest("There is already a display server running on :99")
 
-    with _named_temporary_directory() as tmp:
-        x11 = None
-        xterm = None
-        try:
-            x11 = subprocess.Popen(
-                ['Xorg', '-logfile', './99.log', '-config',
-                 os.path.dirname(__file__) + '/tests/xorg.conf', ':99'],
-                cwd=tmp)
-            while not os.path.exists('/tmp/.X11-unix/X99'):
-                time.sleep(0.1)
-            xterm = subprocess.Popen(
-                ['xterm'], env={'DISPLAY': ':99', 'PATH': os.environ['PATH']},
-                cwd=tmp)
-            # Wait for xterm to get ready to process keyboard input (sorry):
-            time.sleep(1)
-            r = uri_to_remote('x11::99', None)
-            for k in ['t', 'o', 'u', 'c', 'h', 'space', 'g', 'o', 'o', 'd',
-                      'Return', 'e', 'x', 'i', 't', 'Return']:
-                r.press(k)
-            xterm.wait()
-            assert os.path.exists(tmp + '/good')
-        finally:
-            for p in (x11, xterm):
-                if p is not None and p.returncode is None:
-                    p.terminate()
+    with _named_temporary_directory() as tmp, temporary_x_session() as display:
+        r = uri_to_remote('x11:%s' % display, None)
+
+        subprocess.Popen(
+            ['xterm'], env={'DISPLAY': display, 'PATH': os.environ['PATH']},
+            cwd=tmp, stderr=open('/dev/null', 'w'))
+
+        # Can't be sure how long xterm will take to get ready:
+        for _ in range(0, 20):
+            for keysym in ['t', 'o', 'u', 'c', 'h', 'space', 'g', 'o', 'o',
+                           'd', 'Return']:
+                r.press(keysym)
+            if os.path.exists(tmp + '/good'):
+                break
+            time.sleep(0.5)
+        assert os.path.exists(tmp + '/good')
 
 
 def test_uri_to_remote():