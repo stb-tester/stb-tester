86bc557fb4f0e6414a6032d09923d52f73259452
David Röthlisberger <david@rothlis.net>
2014-09-01 08:41:26 +0100

    python packagisation: Move stbt._named_temporary_directory to stbt.utils

--- stbt/__init__.py
+++ stbt/__init__.py
@@ -871,17 +871,6 @@ _ocr_replacements = {
 _ocr_transtab = dict((ord(amb), to) for amb, to in _ocr_replacements.items())
 
 
-@contextmanager
-def _named_temporary_directory(
-        suffix='', prefix='tmp', dir=None):  # pylint: disable=W0622
-    from shutil import rmtree
-    dirname = tempfile.mkdtemp(suffix, prefix, dir)
-    try:
-        yield dirname
-    finally:
-        rmtree(dirname)
-
-
 def _find_tessdata_dir():
     from distutils.spawn import find_executable
 
@@ -977,7 +966,7 @@ def _tesseract(frame, region=Region.ALL,
     # If you give it "hello" the output will be written to "hello.txt", but in
     # hOCR mode it will be "hello.html" (tesseract 3.02) or "hello.hocr"
     # (tesseract 3.03). We work around this with a temporary directory:
-    with _named_temporary_directory(prefix='stbt-ocr-', dir=tmpdir) as tmp:
+    with utils.named_temporary_directory(prefix='stbt-ocr-', dir=tmpdir) as tmp:
         outdir = tmp + '/output'
         os.mkdir(outdir)
 
@@ -3002,7 +2991,7 @@ def temporary_x_session():
     from nose.plugins.skip import SkipTest
     if os.path.exists('/tmp/.X11-unix/X99'):
         raise SkipTest("There is already a display server running on :99")
-    with _named_temporary_directory() as tmp:
+    with utils.named_temporary_directory() as tmp:
         x11 = subprocess.Popen(
             ['Xorg', '-logfile', './99.log', '-config',
              os.path.join(os.path.dirname(__file__), '../tests/xorg.conf'),
@@ -3023,7 +3012,8 @@ def test_x11_remote():
     if not find_executable('Xorg') or not find_executable('xterm'):
         raise SkipTest("Testing X11Remote requires X11 and xterm")
 
-    with _named_temporary_directory() as tmp, temporary_x_session() as display:
+    with utils.named_temporary_directory() as tmp, \
+            temporary_x_session() as display:
         r = uri_to_remote('x11:%s' % display, None)
 
         subprocess.Popen(
--- stbt/utils.py
+++ stbt/utils.py
@@ -1,5 +1,8 @@
 import errno
 import os
+import tempfile
+from contextlib import contextmanager
+from shutil import rmtree
 
 
 def mkdir(d):
@@ -9,3 +12,13 @@ def mkdir(d):
         if e.errno != errno.EEXIST:
             return False
     return os.path.isdir(d) and os.access(d, os.R_OK | os.W_OK)
+
+
+@contextmanager
+def named_temporary_directory(
+        suffix='', prefix='tmp', dir=None):  # pylint: disable=W0622
+    dirname = tempfile.mkdtemp(suffix, prefix, dir)
+    try:
+        yield dirname
+    finally:
+        rmtree(dirname)
