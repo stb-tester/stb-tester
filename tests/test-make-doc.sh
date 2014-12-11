8abd16a7f4f191a176fda5805ce744ac065db6ac
David Röthlisberger <david@rothlis.net>
2014-12-11 22:57:54 +0000

    Fix test_that_readme_is_up_to_date on Travis
    On Travis the test was failing with:
    
        fatal: No names found, cannot describe anything.
        Cloning into 'stb-tester'...
        fatal: attempt to fetch/clone from a shallow repository
        fatal: Could not read from remote repository.
        Please make sure you have the correct access rights
        and the repository exists.
        error: Failed to run 'update-api-docs'
    
    It seems that the stb-tester checkout ($srcdir in the test) is a shallow
    clone, and it seems that the version of git on travis can't clone from a
    shallow clone.
    
    This commit copies the entire stb-tester checkout instead of cloning it,
    and it uses `diff` instead of `git diff` to detect changes. As a nice
    side effect, the test is now valid even if you're running the test on
    uncommitted changes.

--- tests/test-make-doc.sh
+++ tests/test-make-doc.sh
@@ -1,17 +1,11 @@
 # Run with ./run-tests.sh
 
 test_that_readme_is_up_to_date() {
-    if git -C "$srcdir" describe --dirty | grep -q dirty; then
-        skip "source directory has uncommitted changes"
-    fi
-
-    git clone "$srcdir" stb-tester &&
+    cp -R "$srcdir" stb-tester &&
     cd stb-tester &&
     make update-api-docs ||
     fail "Failed to run 'update-api-docs'"
 
-    [[ -z "$(git status --porcelain -- README.rst)" ]] || {
-        git diff
-        fail "README.rst is not up to date; run 'make update-api-docs'"
-    }
+    diff -u "$srcdir"/README.rst README.rst ||
+    fail "README.rst is not up to date; run 'make update-api-docs'"
 }
