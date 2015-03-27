stb-tester/stb-tester

commit 531be6496c43f68bacb6beeaacf275ce4db1e188

Makefile: [fix] allow minor releases for RPMs
Previously, incrementing the Release number in stb-tester.spec.in to
anything other than 1 would cause the srpm make target to break because
the name of the produced RPM would not match that which was hardcoded to
the "src_rpm" variable.

This commit fixes the issue by substituting the minor release into the
RPM spec file in the same way that the major version is. Users wishing to
create minor releases can now either hardcode the increment into the
Makefile by changing the assignment to the `RELEASE` variable or simply
assign it on the `make` command line, e.g. `make rpm RELEASE=3`.

Makefile
@@ -49,6 +49,7 @@ generate_version := $(shell \
 	rm -f VERSION.now)
 VERSION?=$(shell cat VERSION)
 ESCAPED_VERSION=$(subst -,_,$(VERSION))
+RELEASE?=1
 
 .DELETE_ON_ERROR:
 
@@ -59,6 +60,7 @@ extra/debian/changelog extra/fedora/stb-tester.spec stbt.sh: \
   %: %.in .stbt-prefix VERSION
 	sed -e 's,@VERSION@,$(VERSION),g' \
 	    -e 's,@ESCAPED_VERSION@,$(ESCAPED_VERSION),g' \
+	    -e 's,@RELEASE@,$(RELEASE),g' \
 	    -e 's,@LIBEXECDIR@,$(libexecdir),g' \
 	    -e 's,@SYSCONFDIR@,$(sysconfdir),g' \
 	    -e "s/@RFC_2822_DATE@/$$(git show -s --format=%aD HEAD)/g" \
@@ -277,7 +279,7 @@ stb-tester_$(VERSION)-%_$(debian_architecture).deb: \
 ### Fedora Packaging #########################################################
 
 rpm_topdir?=$(HOME)/rpmbuild
-src_rpm=stb-tester-$(ESCAPED_VERSION)-1.fc20.src.rpm
+src_rpm=stb-tester-$(ESCAPED_VERSION)-$(RELEASE).fc20.src.rpm
 
 srpm: $(src_rpm)
 

extra/fedora/stb-tester.spec.in
@@ -1,6 +1,6 @@
 Name: stb-tester
 Version: @ESCAPED_VERSION@
-Release: 1%{?dist}
+Release: @RELEASE@%{?dist}
 Summary: Automated user interface testing for set-top boxes
 Group: Development/Tools
 URL: http://stb-tester.com

commit 34ec234d5de26c64049bf124fdd30269a264fe58

Fix ubuntu packaging, broken in 531be649
After 531be649, `extra/debian/changelog` said "stb-tester (22-1)"
instead of "stb-tester (22-@RELEASE@)" (@RELEASE@ was supposed to
be filled in later by `extra/debian/build-source-package.sh`).

This change moves the creation of `changelog` from `changelog.in`
entirely out of the Makefile and into `build-source-package.sh`. (Before
it was partly in the Makefile and partly in `build-source-package.sh`.)

.gitignore
@@ -9,7 +9,6 @@
 .stbt-prefix
 debian-packages
 defaults.conf
-extra/debian/changelog
 extra/fedora/stb-tester.spec
 extra/stb-tester*.debian.tar.xz
 extra/vm/.vagrant

Makefile
@@ -12,10 +12,6 @@ mandir?=$(datarootdir)/man
 man1dir?=$(mandir)/man1
 sysconfdir?=$(prefix)/etc
 
-user_name?=$(shell git config user.name || \
-                   getent passwd `whoami` | cut -d : -f 5 | cut -d , -f 1)
-user_email?=$(shell git config user.email || echo "$$USER@$$(hostname)")
-
 # Support installing GStreamer elements under $HOME
 gsthomepluginsdir=$(if $(XDG_DATA_HOME),$(XDG_DATA_HOME),$(HOME)/.local/share)/gstreamer-1.0/plugins
 gstsystempluginsdir=$(shell pkg-config --variable=pluginsdir gstreamer-1.0)
@@ -57,16 +53,13 @@ RELEASE?=1
 
 all: stbt.sh stbt.1 defaults.conf extra/fedora/stb-tester.spec
 
-extra/debian/changelog extra/fedora/stb-tester.spec stbt.sh: \
+extra/fedora/stb-tester.spec stbt.sh: \
   %: %.in .stbt-prefix VERSION
 	sed -e 's,@VERSION@,$(VERSION),g' \
 	    -e 's,@ESCAPED_VERSION@,$(ESCAPED_VERSION),g' \
 	    -e 's,@RELEASE@,$(RELEASE),g' \
 	    -e 's,@LIBEXECDIR@,$(libexecdir),g' \
 	    -e 's,@SYSCONFDIR@,$(sysconfdir),g' \
-	    -e "s/@RFC_2822_DATE@/$$(git show -s --format=%aD HEAD)/g" \
-	    -e 's,@USER_NAME@,$(user_name),g' \
-	    -e 's,@USER_EMAIL@,$(user_email),g' \
 	     $< > $@
 
 defaults.conf: stbt.conf .stbt-prefix
@@ -264,7 +257,7 @@ ppa-publish-%: debian-packages/stb-tester_$(VERSION)-%.dsc
 
 # Build debian source package
 debian-packages/stb-tester_$(VERSION)-%.dsc: \
-  stb-tester-$(VERSION).tar.gz extra/debian/changelog
+  stb-tester-$(VERSION).tar.gz extra/debian/changelog.in
 	extra/debian/build-source-package.sh $(VERSION) $*
 
 # Build debian binary package from source package

extra/debian/build-source-package.sh
@@ -53,8 +53,14 @@ cp $srcdir/extra/debian/compat \
     $srcdir/extra/debian/rules \
     $builddir/stb-tester-$version/debian
 cp $srcdir/extra/debian/source/format $builddir/stb-tester-$version/debian/source
-sed -e "s/@RELEASE@/$release/g" -e "s/@DISTRIBUTION@/$distribution/g" \
-    $srcdir/extra/debian/changelog > $builddir/stb-tester-$version/debian/changelog
+sed -e "s/@VERSION@/$version/g" \
+    -e "s/@RELEASE@/$release/g" \
+    -e "s/@DISTRIBUTION@/$distribution/g" \
+    -e "s/@RFC_2822_DATE@/$(git -C "$srcdir" show -s --format=%aD HEAD)/g" \
+    -e "s/@USER_NAME@/$(git -C "$srcdir" config user.name)/g" \
+    -e "s/@USER_EMAIL@/$(git -C "$srcdir" config user.email)/g" \
+    $srcdir/extra/debian/changelog.in \
+    > $builddir/stb-tester-$version/debian/changelog
 
 # .dsc & .debian.tar.gz (both created by debuild).
 (cd $builddir/stb-tester-$version &&