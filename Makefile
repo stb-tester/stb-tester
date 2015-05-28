stb-tester/stb-tester

commit 6890be9ec4775e504f0e1a93cb475a3f06c3dd68

Prepare to release version 22: Allow tags to be prefixes with "v"
We're changing to a new versioning scheme, dropping the "0." from the
version numbers as it is redundant and misleading WRT how much we care
about backwards compatibility, etc.  Future tags will take the form
"v22", "v23", etc. rather than "0.22".  The "v" prefix allows the tag
name to be distinguished from a git SHA.

Makefile
@@ -45,6 +45,7 @@ tools += stbt-tv
 # VERSION file included in the dist tarball otherwise.
 generate_version := $(shell \
 	GIT_DIR=.git git describe --always --dirty > VERSION.now 2>/dev/null && \
+	sed --in-place "s/^v//g" VERSION.now && \
 	{ cmp VERSION.now VERSION 2>/dev/null || mv VERSION.now VERSION; }; \
 	rm -f VERSION.now)
 VERSION?=$(shell cat VERSION)

commit 18c774532478f47347bc71989c38fea0bc9a9996

Makefile: Fix `make` on OS X
Broken in 6890be9e shortly before the v22 release.

BSD sed doesn't have the long form "--in-place", and the short "-i" has
incompatible behaviour between GNU sed and BSD sed.

Makefile
@@ -41,7 +41,7 @@ tools += stbt-tv
 # VERSION file included in the dist tarball otherwise.
 generate_version := $(shell \
 	GIT_DIR=.git git describe --always --dirty > VERSION.now 2>/dev/null && \
-	sed --in-place "s/^v//g" VERSION.now && \
+	perl -pi -e 's/^v//' VERSION.now && \
 	{ cmp VERSION.now VERSION 2>/dev/null || mv VERSION.now VERSION; }; \
 	rm -f VERSION.now)
 VERSION?=$(shell cat VERSION)