stb-tester/stb-tester

commit 05f23c1c315f86cc29c211bf5d25d969cd4f6a05

extra/fedora/copr-publish.sh: Only run `copr-cli` in Fedora container
The `copr-cli` tool is only available in Fedora, so it needs to be run
in a Fedora container. Now that the `git clone` is done on the host, I
can use git's ssh transport (I don't have to type in a password) without
having to figure out how to get the credentials into the container.

extra/fedora/copr-publish.sh
@@ -13,15 +13,14 @@ set -x
 [[ -n "$src_rpm" ]] &&
 tmpdir=$(mktemp -d --tmpdir stb-tester-copr-publish.XXXXXX) &&
 trap "rm -rf $tmpdir" EXIT &&
-git clone --depth 1 https://github.com/stb-tester/stb-tester-srpms.git \
+git clone --depth 1 git@github.com:stb-tester/stb-tester-srpms.git \
     $tmpdir/stb-tester-srpms &&
 cp $src_rpm $tmpdir/stb-tester-srpms &&
-cd $tmpdir/stb-tester-srpms &&
-git add $src_rpm &&
-git commit -m "$src_rpm" &&
-git push origin master &&
+git -C $tmpdir add $src_rpm &&
+git -C $tmpdir commit -m "$src_rpm" &&
+git -C $tmpdir push origin master &&
 echo "Published srpm to https://github.com/stb-tester/stb-tester-srpms" &&
-copr-cli build stb-tester \
-    https://github.com/stb-tester/stb-tester-srpms/raw/master/$src_rpm &&
+"$(dirname "$0")"/fedora-shell.sh -c "copr-cli build stb-tester \
+    https://github.com/stb-tester/stb-tester-srpms/raw/master/$src_rpm" &&
 echo "Kicked off copr build" &&
 echo "See http://copr.fedoraproject.org/coprs/stbt/stb-tester/"

commit f70a21c1a3085c5a1214549535adb4e362665d4f

extra/fedora: Fix copr-publish.sh, broken in 05f23c1

extra/fedora/copr-publish.sh
@@ -16,9 +16,9 @@ trap "rm -rf $tmpdir" EXIT &&
 git clone --depth 1 git@github.com:stb-tester/stb-tester-srpms.git \
     $tmpdir/stb-tester-srpms &&
 cp $src_rpm $tmpdir/stb-tester-srpms &&
-git -C $tmpdir add $src_rpm &&
-git -C $tmpdir commit -m "$src_rpm" &&
-git -C $tmpdir push origin master &&
+git -C $tmpdir/stb-tester-srpms add $src_rpm &&
+git -C $tmpdir/stb-tester-srpms commit -m "$src_rpm" &&
+git -C $tmpdir/stb-tester-srpms push origin master &&
 echo "Published srpm to https://github.com/stb-tester/stb-tester-srpms" &&
 "$(dirname "$0")"/fedora-shell.sh -c "copr-cli build stb-tester \
     https://github.com/stb-tester/stb-tester-srpms/raw/master/$src_rpm" &&