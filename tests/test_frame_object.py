11baadafa274333665c33a14764a2205d3bcd672
William Manley <will@stb-tester.com>
2016-03-07 15:26:03 +0000

    FrameObject: Memoize all properties
    Frame Objects are immutable, so memoization can be used without having an
    effect on the outward behaviour of the objects, apart from speeding them
    up.  This enables usage like
    
        assert o.text.startswith('hello') and o.text.endswith('goodbye')
    
    rather than using an intermediate variable for `o.text` to achieve
    reasonable performance..

--- tests/test_frame_object.py
+++ tests/test_frame_object.py
@@ -16,6 +16,43 @@ class TruthyFrameObject(stbt.FrameObject):
         return True
 
 
+class PrintingFrameObject(stbt.FrameObject):
+    """
+    This is a very naughty FrameObject.  It's properties cause side-effects so
+    we can check that the caching is working:
+
+    >>> frame = _load_frame("with-dialog")
+    >>> m = PrintingFrameObject(frame)
+    >>> m.is_visible
+    is_visible called
+    _helper called
+    True
+    >>> m.is_visible
+    True
+    >>> m.another
+    another called
+    10
+    >>> m.another
+    10
+    >>> m
+    PrintingFrameObject(is_visible=True, another=10)
+    """
+    @property
+    def is_visible(self):
+        print "is_visible called"
+        return bool(self._helper)
+
+    @property
+    def _helper(self):
+        print "_helper called"
+        return 7
+
+    @property
+    def another(self):
+        print "another called"
+        return self._helper + 3
+
+
 def _load_frame(name):
     import cv2
     from os.path import abspath, dirname
