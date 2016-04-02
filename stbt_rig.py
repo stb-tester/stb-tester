ceafdc2a9a11dae702281035ef083aa3d66593c4
William Manley <will@stb-tester.com>
2016-04-02 11:51:00 +0100

    stbt rig: Add stbt rig sync
    Synchronises local git repo's working directory with the one running on
    the stb-tester appliance.  This should make trying things out and
    developing remotely more convenient.

--- stbt_rig.py
+++ stbt_rig.py
@@ -1,12 +1,105 @@
 import argparse
 import subprocess
 import sys
+import urlparse
 
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.parse_args(argv[1:])
+    parser.add_argument(
+        "--appliance", help="The URL of your stb-tester ONE appliance")
+
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.add_parser(
+        'sync', help="Synchronise your working directory with an appliance")
+
+    cmdline_args = parser.parse_args(argv[1:])
+
+    if cmdline_args.appliance is not None:
+        u = urlparse.urlparse(cmdline_args.appliance)
+        if u.scheme == 'http':
+            hostname = u.netloc
+        elif u.scheme == '':
+            hostname = cmdline_args.appliance
+        else:
+            sys.stderr.write(
+                "Appliance argument must be of the form "
+                "http://<hostname>[:port]/ or <hostname>.  Did not understand "
+                "%r\n" % cmdline_args.appliance)
+            return 1
+    else:
+        # In the future we can try harder to find out the upstream hostname,
+        # e.g. using git remotes or zeroconf
+        sys.stderr.write("No appliance name specified\n")
+        return 1
+
+    if cmdline_args.command == 'sync':
+        if cmdline_args.appliance:
+            m = re.match('http://(.*)/.*')
+            hostname = m.group(1)
+        sync(cmdline_args.appliance)
     return 1
+
+
+def sync(hostname):
+    remote = "http://%s/git/test-pack.git" % hostname
+    repo = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'])
+    remote_branch_name = "sync/" + subprocess.check_output(
+        ['git', 'config', 'user.email'], curdir=repo)
+
+    def update():
+        commit_sha = git_snapshot(repo)
+        subprocess.check_output(['git', 'push', '--force', remote, '%s:%s' % (
+            commit_sha, remote_branch_name)], curdir=repo)
+
+    try:
+        update()
+        with watch(repo) as filesystem_events:
+            for _ in debounce(filesystem_events):
+                update()
+    finally:
+        subprocess.check_output(
+            ['git', 'push', remote, ':%s' % remote_branch_name])
+
+
+def debounce(iterable, timeout=1):
+    last_event_time = time.time()
+    for x in iterable:
+        event_time = time.time()
+        if event_time > last_event_time + timeout:
+            yield True
+        last_event_time = event_time
+
+
+@contextmanager
+def watch(directory):
+    import watchdog
+    from watchdog.observers import Observer
+    import Queue
+
+    q = Queue.Queue()
+
+    def queue_iter():
+        while True:
+            end, item = q.get()
+            if end:
+                return
+            else:
+                yield item
+
+    class QueuingEventHandler(watchdog.events.FileSystemEvent):
+        def on_any_event(self, event):
+            q.put((False, event))
+
+    observer = watchdog.observers.Observer()
+    observer.schedule(QueuingEventHandler(), directory, recursive=True)
+    try:
+        observer.start()
+        yield q
+    finally:
+        q.put((True, None))
+        observer.stop()
+    observer.join()
 
 
 class UnknownFileInRepo(Exception):
