c87c71887326af1bbad10c1372b13bdb6ac8bbce
William Manley <will@stb-tester.com>
2016-05-26 18:12:54 +0100

    HdmiCecControl: Add scaffolding for HdmiCecControl implementation
    To be filled in later by an stb-tester contributor.

--- _stbt/control_gpl.py
+++ _stbt/control_gpl.py
@@ -1,2 +1,18 @@
+class HdmiCecControl(object):
+    def __init__(self, device):
+        import cec
+        raise NotImplementedError()
+
+    def press(self, key):
+        import cec
+        raise NotImplementedError()
+
+
+def test_hdmi_cec_control():
+    from .control import uri_to_remote
+    control = uri_to_remote('hdmi-cec:192.168.1.3')
+    assert False, "Tests not written"
+
 controls = [
+    (r'hdmi-cec:(?P<device>[^:]+)', HdmiCecControl),
 ]
