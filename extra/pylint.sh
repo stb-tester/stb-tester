c1c9b543a3176a874f47841892059bfad5e86170
David Röthlisberger <david@rothlis.net>
2017-01-14 14:48:28 +0000

    extra/pylint.sh: Disable pep8 "closing bracked indentation" check
    It's inconsistent across versions of the pep8 tool, impossible to
    satisfy both Travis (Ubuntu 14.04) and my dev laptop (Ubuntu 16.04).

--- extra/pylint.sh
+++ extra/pylint.sh
@@ -9,11 +9,12 @@
 [ $# -gt 0 ] || { grep '^#/' "$0" | cut -c4- >&2; exit 1; }
 
 pep8options() {
+    # E124: closing bracket does not match visual indentation
     # E402: module level import not at top of file (because isort does it)
     # E501: line too long > 80 chars (because pylint does it)
     # E721: do not compare types, use 'isinstance()' (because pylint does it)
     # E731: do not assign a lambda expression, use a def
-    echo --ignore=E402,E501,E721,E731
+    echo --ignore=E124,E402,E501,E721,E731
 }
 
 ret=0
