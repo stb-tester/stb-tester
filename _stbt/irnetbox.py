ccff23e7a0441d2b4dfca0591c77a60c20de5f14
hbovta <martin.siden@kentor.se>
2019-01-24 13:33:13 +0100

    Added support for RedRat X
    Made some small modifications to irnetbox.py making it possible to
    use a RedRat X IR blaster with stbt. This basically just involved
    adding the RRX PID (13) to class NetBoxTypes and modifying the if/else
    in method irsend_raw.
    
    This will treat the RRX same as an IRNetBox allowing for communication
    with the device using ethernet. Please note that it will NOT support
    the RRX's USB interface.
    
    I have tested this implementation thoroughly on my side and find that
    it works well. According to the people at RedRat chances are good it
    will also work with IRNetBox 4 but I have been unable to verify this.

--- _stbt/irnetbox.py
+++ _stbt/irnetbox.py
@@ -122,14 +122,14 @@ class IRNetBox(object):
     def irsend_raw(self, port, power, data):
         """Output the IR data on the given port at the set power (§6.1.1).
 
-        * `port` is a number between 1 and 16.
+        * `port` is a number between 1 and 16 (or 1 and 4 for if you yse a RedRat X).
         * `power` is a number between 1 and 100.
         * `data` is a byte array as exported by the RedRat Signal DB Utility.
 
         """
         if self.irnetbox_model == NetBoxTypes.MK1:
             raise Exception("IRNetBox MK1 not supported")
-        elif self.irnetbox_model == NetBoxTypes.MK2:
+        elif self.irnetbox_model in (NetBoxTypes.MK2, NetBoxTypes.RRX, NetBoxTypes.MK4):
             self.reset()
             self.indicators_on()
             self._send(MessageTypes.SET_MEMORY)
@@ -226,6 +226,8 @@ class NetBoxTypes(object):
     MK1 = 2
     MK2 = 7
     MK3 = 8
+    MK4 = 12
+    RRX = 13
 
 
 def _message(message_type, message_data):
@@ -365,3 +367,4 @@ class _FileToSocket(object):
 
     def recv(self, bufsize, flags=0):  # pylint:disable=unused-argument
         return self.file.read(bufsize)
+
