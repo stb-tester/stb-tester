a43d218b6ba4eef892c268b3311fb9c7e0bdc302
David Röthlisberger <david@stb-tester.com>
2019-05-07 14:06:27 +0100

    Add import guards (if __name__ == "__main__")
    ...to the files renamed to '*.py' in the previous commit. This allows
    pytest test-collection to work.

--- stbt_config.py
+++ stbt_config.py
@@ -17,33 +17,38 @@ def error(s):
     sys.exit(1)
 
 
-parser = argparse.ArgumentParser()
-parser.prog = "stbt config"
-parser.description = """Prints the value of the specified key from the stbt
-    configuration file. See 'configuration' in the stbt(1) man page."""
-parser.epilog = \
-    "Returns non-zero exit status if the specified key or section isn't found."
-parser.add_argument(
-    "--bash-completion", action="store_true", help=argparse.SUPPRESS)
-parser.add_argument(
-    "name", metavar="section.key",
-    help="e.g. 'global.source_pipeline' or 'record.control_recorder'")
-args = parser.parse_args(sys.argv[1:])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.prog = "stbt config"
+    parser.description = """Prints the value of the specified key from the stbt
+        configuration file. See 'configuration' in the stbt(1) man page."""
+    parser.epilog = """Returns non-zero exit status if the specified key or
+        section isn't found."""
+    parser.add_argument(
+        "--bash-completion", action="store_true", help=argparse.SUPPRESS)
+    parser.add_argument(
+        "name", metavar="section.key",
+        help="e.g. 'global.source_pipeline' or 'record.control_recorder'")
+    args = parser.parse_args(sys.argv[1:])
 
-if args.bash_completion:
-    cfg = _config_init()
-    for section in cfg.sections():
-        for option in cfg.options(section):
-            print "%s.%s" % (section, option)
-    sys.exit(0)
+    if args.bash_completion:
+        cfg = _config_init()
+        for section in cfg.sections():
+            for option in cfg.options(section):
+                print "%s.%s" % (section, option)
+        sys.exit(0)
 
-if args.name.rfind(".") == -1:
-    error(
-        "'name' parameter must contain the section and key separated by a dot")
+    if args.name.rfind(".") == -1:
+        error("'name' parameter must contain the section and key "
+              "separated by a dot")
 
-section, key = args.name.rsplit(".", 1)
+    section, key = args.name.rsplit(".", 1)
 
-try:
-    print get_config(section, key)
-except ConfigurationError as e:
-    error(e.message)
+    try:
+        print get_config(section, key)
+    except ConfigurationError as e:
+        error(e.message)
+
+
+if __name__ == "__main__":
+    main()
