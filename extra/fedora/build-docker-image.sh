347b080148ed9121faa7026e276910fde06eb3ac
David Röthlisberger <david@stb-tester.com>
2019-05-13 12:02:01 +0100

    Remove obsolete Fedora packaging scripts
    We haven't built Fedora packages since 3 years ago.

--- extra/fedora/build-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0) &&
-
-buildrequires=$(awk '/^BuildRequires:/ {printf "%s ",$2}' stb-tester.spec.in) &&
-cat Dockerfile.in |
-sed "s/@BUILDREQUIRES@/$buildrequires/" |
-docker build -t stbtester/stb-tester-fedora-build-environment -
