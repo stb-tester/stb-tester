5c8e8c519d134bd6693e91efb6a6eaa44017d239
David Röthlisberger <david@stb-tester.com>
2019-07-16 13:02:26 +0100

    stbt.press_and_wait: Ignore pixel intensity differences <= 5
    On the Apple TV, successive frames differ by up to 3 (out of 255)
    on almost every pixel, in a funky multicoloured moire pattern.

--- _stbt/transition.py
+++ _stbt/transition.py
@@ -246,10 +246,11 @@ def strict_diff(prev, frame, region, mask_image):
         _ddebug("found %s diffs above 20 (max %s) in %r", frame,
                 numpy.count_nonzero(big_diffs), maxdiff, out_region)
     elif maxdiff > 0:
-        small_diffs_count = numpy.count_nonzero(absdiff)
+        small_diffs = absdiff > 5
+        small_diffs_count = numpy.count_nonzero(small_diffs)
         if small_diffs_count > 50:
             diffs_found = True
-            out_region = pixel_bounding_box(absdiff)
+            out_region = pixel_bounding_box(small_diffs.max(axis=2))
             _ddebug("found %s diffs <= %s in %r", frame, small_diffs_count,
                     maxdiff, out_region)
     if out_region:
