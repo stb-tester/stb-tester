c982b56ac80c1bb1ad384022bbe416de3d0e2031
David Röthlisberger <david@stb-tester.com>
2020-03-19 12:59:57 +0000

    test_that_press_returns_a_pressresult: Rename `result` variable
    To `result1`, `result2`, and `result3`, to be more explicit. This will
    make the test in my next commit clearer.

--- tests/test-stbt-py.sh
+++ tests/test-stbt-py.sh
@@ -600,29 +600,29 @@ test_that_press_returns_a_pressresult() {
 	import time
 	
 	before = time.time()
-	result = stbt.press("KEY_MENU")
+	result1 = stbt.press("KEY_MENU")
 	after = time.time()
 
-	assert before < result.start_time < result.end_time < after
-	assert result.key == "KEY_MENU"
-	assert isinstance(result.frame_before, stbt.Frame)
+	assert before < result1.start_time < result1.end_time < after
+	assert result1.key == "KEY_MENU"
+	assert isinstance(result1.frame_before, stbt.Frame)
 
 	before = time.time()
-	result = stbt.press("KEY_HOME", hold_secs=0.001)
+	result2 = stbt.press("KEY_HOME", hold_secs=0.001)
 	after = time.time()
 
-	assert before < result.start_time < result.end_time < after
-	assert result.key == "KEY_HOME"
-	assert isinstance(result.frame_before, stbt.Frame)
+	assert before < result2.start_time < result2.end_time < after
+	assert result2.key == "KEY_HOME"
+	assert isinstance(result2.frame_before, stbt.Frame)
 
 	before = time.time()
-	with stbt.pressing("KEY_VOLUMEUP") as result:
-	    assert before < result.start_time
-	    assert result.end_time is None
-	    assert result.key == "KEY_VOLUMEUP"
-	    assert isinstance(result.frame_before, stbt.Frame)
+	with stbt.pressing("KEY_VOLUMEUP") as result3:
+	    assert before < result3.start_time
+	    assert result3.end_time is None
+	    assert result3.key == "KEY_VOLUMEUP"
+	    assert isinstance(result3.frame_before, stbt.Frame)
 	after = time.time()
-	assert result.start_time < result.end_time < after
+	assert result3.start_time < result3.end_time < after
 	EOF
     stbt run -vv --control=none test.py || fail "Incorrect press() behaviour"
 }
