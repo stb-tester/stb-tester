5caf52ba19b106f776e69aa3e0fdd153c2e2ea47
David Röthlisberger <david@stb-tester.com>
2020-08-10 17:37:10 +0100

    Fix `from stbt import *` with Python 2.7
    This has been broken since we added Python3 compatibility. Due to `from
    __future__ import unicode_literals`, attempting to do `from stbt
    import *` fails with:
    
        TypeError: Item in ``from list'' must be str, not unicode
    
    Of course `__unicode_literals__` must go at the top of the file, so you
    can't move it to below `__all__`.
    
    If I add a `b` literal to each element, then Python 3.6 fails with:
    
        TypeError: Item in stbt.__all__ must be str, not bytes

--- stbt_core/__init__.py
+++ stbt_core/__init__.py
@@ -72,10 +72,12 @@ from _stbt.types import (
     Region,
     UITestError,
     UITestFailure)
+from _stbt.utils import (
+    to_native_str)
 from _stbt.wait import (
     wait_until)
 
-__all__ = [
+__all__ = [to_native_str(x) for x in [
     "apply_ocr_corrections",
     "as_precondition",
     "ConfigurationError",
@@ -125,7 +127,7 @@ __all__ = [
     "wait_for_motion",
     "wait_for_transition_to_end",
     "wait_until",
-]
+]]
 
 # Functions available to stbt scripts
 # ===========================================================================
