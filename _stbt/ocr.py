stb-tester/stb-tester

commit 5ecd51f51517fdbc000bdfa5d45272eae65ce78a

stbt.ocr corrections: Match non-word characters at string boundaries
When you specify a plain string as an OCR correction, it's supposed to
match at a word boundary (if you want anything more complex, you can
specify a compiled regular expression). `re` defines word boundaries as
going from a word character to a non-word character (the start or end of
the string count as non-word characters). But if your correction string
starts or ends with a non-word character, we don't consider the
transition from that (non-word) character, to another non-word
character, as a match.

The goal of this logic is to replace complete words or phrases without
the surprising behaviour of replacing sub-strings inside another longer
word. So I have re-defined "word boundary" to mean whitespace or the end
of the string. I think that achieves our goal, while also fixing this
bug.

This whole feature was severely under-tested. I have added some other
test-cases, too, that caught bugs in my initial attempts at this fix.

_stbt/ocr.py
@@ -399,26 +399,22 @@ def apply_ocr_corrections(text, corrections=None):
 def _apply_ocr_corrections(text, corrections):
 
     def replace_string(matchobj):
-        old = matchobj.group(0)
+        old = matchobj.group(2)
         new = corrections[old]
         debug("ocr corrections: %r -> %r" % (old, new))
-        return new
+        return matchobj.group(1) + new + matchobj.group(3)
 
     def replace_regex(matchobj):
         new = corrections[matchobj.re]
         debug("ocr corrections: /%s/ -> %r" % (matchobj.re.pattern, new))
         return new
 
-    # Match plain strings at word boundaries:
-    pattern = "|".join(r"\b(" + re.escape(k) + r")\b"
-                       for k in corrections
-                       if isinstance(k, str))
-    if pattern:
-        text = re.sub(pattern, replace_string, text)
-
-    # Match regexes:
     for k in corrections:
-        if isinstance(k, PatternType):
+        if isinstance(k, str):
+            # Match plain strings at word boundaries
+            text = re.sub(r"(^|\s)(" + re.escape(k) + r")(\s|$)",
+                          replace_string, text)
+        elif isinstance(k, PatternType):
             text = re.sub(k, replace_regex, text)
     return text
 

tests/test_ocr.py
@@ -182,8 +182,7 @@ def test_char_whitelist():
         char_whitelist="0123456789")
 
 
-@requires_tesseract
-@pytest.mark.parametrize("corrections,expected", [
+corrections_test_cases = [
     # pylint:disable=bad-whitespace
     # Default ocr output:
     (None,                                           'OO'),
@@ -197,7 +196,11 @@ def test_char_whitelist():
     # Make sure it tries all the patterns:
     ({'AA': 'BB', 'OO': '00'},                       '00'),
     ({re.compile('^O'): '1', re.compile('O$'): '2'}, '12'),
-])
+]
+
+
+@requires_tesseract
+@pytest.mark.parametrize("corrections,expected", corrections_test_cases)
 def test_corrections(corrections, expected):
     f = load_image('ocr/00.png')
     print(corrections)
@@ -212,6 +215,21 @@ def test_corrections(corrections, expected):
                                     corrections=corrections)
     finally:
         stbt.set_global_ocr_corrections({})
+
+
+@pytest.mark.parametrize(
+    "text,corrections,expected",
+    # Same test-cases as `test_corrections` above:
+    [('OO', c, e) for (c, e) in corrections_test_cases] +
+    # Plain strings match entire words at word boundaries:
+    [('itv+', {'itv+': 'Apple tv+'}, 'Apple tv+'),
+     ('hitv+', {'itv+': 'Apple tv+'}, 'hitv+'),
+     ('This is itv+ innit', {'itv+': 'Apple tv+'}, 'This is Apple tv+ innit'),
+     # Make sure it tries all the patterns:
+     ('A B C', {'A': '1', 'B': '2', 'C': '3'}, '1 2 3'),
+    ])
+def test_apply_ocr_corrections(text, corrections, expected):
+    assert expected == stbt.apply_ocr_corrections(text, corrections)
 
 
 @requires_tesseract

commit ae01a7f71379c2715f08ed2544d16771865f6710

stbt.ocr corrections: Fix matching word boundary
This fixes an (unreleased) regression introduced in commit 5ecd51f
(stbt.ocr corrections: Match non-word characters at string boundaries).

Spaces aren't the only word boundaries.

_stbt/ocr.py
@@ -411,8 +411,8 @@ def _apply_ocr_corrections(text, corrections):
     for k in corrections:
         if isinstance(k, str):
             # Match plain strings at word boundaries
-            text = re.sub(r"(^|\s)(" + re.escape(k) + r")(\s|$)",
-                          replace_string, text)
+            text = re.sub(r"(^|\W)(" + re.escape(k) + r")(\W|$)",
+                          replace_string, text, re.UNICODE)
         elif isinstance(k, PatternType):
             text = re.sub(k, replace_regex, text)
     return text

tests/test_ocr.py
@@ -221,10 +221,13 @@ def test_corrections(corrections, expected):
     "text,corrections,expected",
     # Same test-cases as `test_corrections` above:
     [('OO', c, e) for (c, e) in corrections_test_cases] +
-    # Plain strings match entire words at word boundaries:
+    # Plain strings match entire words at word boundaries, regexes don't:
     [('itv+', {'itv+': 'Apple tv+'}, 'Apple tv+'),
      ('hitv+', {'itv+': 'Apple tv+'}, 'hitv+'),
      ('This is itv+ innit', {'itv+': 'Apple tv+'}, 'This is Apple tv+ innit'),
+     ('the saw said he saws, he saw.',
+      {'he saw': 'HE SAW', re.compile("he", re.IGNORECASE): "ħé"},
+      'tħé saw said ħé saws, ħé SAW.'),
      # Make sure it tries all the patterns:
      ('A B C', {'A': '1', 'B': '2', 'C': '3'}, '1 2 3'),
     ])