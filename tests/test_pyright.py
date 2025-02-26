caab8adf9520167d9f59c1958acc720cbfe12a40
David Röthlisberger <david@stb-tester.com>
2025-02-26 13:37:55 +0000

    tests: Regression test for Region.bounding_box typing

---
+++ tests/test_pyright.py
@@ -0,0 +1,15 @@
+# This file doesn't contain any runnable tests, but it is linted by
+# `make check-pyright` to catch any type warnings.
+
+import stbt_core as stbt
+
+# pylint:disable=line-too-long
+
+
+def f():
+    lines = [
+        stbt.Region(255, 390, right=1280, height=45),
+        stbt.Region(255, 443, right=1280, height=45),
+    ]
+    all_lines = stbt.Region.bounding_box(*lines)
+    print(all_lines.width)  # "width" is not a known attribute of "None" (reportOptionalMemberAccess)
